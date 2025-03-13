c0923d64112a08f457fd8aa92a36c5eee1743380
Kathan-Prajapati <kathanb146@gmail.com>
2025-03-13 14:35:07 +0530

    Add files via upload

---
+++ lib/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'auth_service.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Provider.of<AuthService>(context, listen: false).signOut();
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+            },
+          ),
+        ],
+      ),
+      body: Center(child: Text("Welcome to the Airline Booking System")),
+    );
+  }
+}
